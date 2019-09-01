ab5d22d03f4cfbfa2e1bcb6763334483e82f1228
huewilliams <4879sinwoo@naver.com>
2019-09-01 09:42:02 +0900

    Fix: room code

--- src/routes/room.js
+++ src/routes/room.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Room = require('../models').Room;
+
 router.get('/', async (req, res, next) => {
     const room = await Room.findAll();
     if(room) res.json(room);
